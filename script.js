c3e29bd68310b8feffed1eb28cc113dc9319f480
NickZen0 <127402370+NickZen0@users.noreply.github.com>
2024-04-04 22:47:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function humanas(){
+    window.location.href = "/logado/humanas.html"
+}
+function matematica(){
+    window.location.href = "/logado/matematica.html"
+}
+function linguagens(){
+    window.location.href = "/logado/linguagens.html"
+}
+function natureza(){
+    window.location.href = "/logado/natureza.html"
+}
+function chapadonic(){
+    window.location.href = "index.html"
+}
+function apre(){
+    window.location.href = "apre.html"
+}
